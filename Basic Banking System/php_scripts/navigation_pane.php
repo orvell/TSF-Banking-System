0f161adcea4de499602be7dc833e065a2e2b9b8d
ORVELL FERREIRA <56030574+orvell@users.noreply.github.com>
2021-04-06 10:56:23 +0530

    Update navigation_pane.php

--- Basic Banking System/php_scripts/navigation_pane.php
+++ Basic Banking System/php_scripts/navigation_pane.php
@@ -10,8 +10,8 @@
       <a href="../index.php"><img src="../images/logo.png" class="logo-image"></img></a>
       <ul class="nav-area">
         <li><a href="../index.php">Home</a></li>
-        <li><a href="transfer_money.php">Transfer Money</a></li>
         <li><a href="view_customers.php">View Customers</a></li>
+        <li><a href="transfer_money.php">Transfer Money</a></li>
         <li><a href="transactions.php">Transactions</a></li>
 
       </ul>
